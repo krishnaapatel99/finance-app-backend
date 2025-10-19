9f03f60e1fd344a1c3267af875f9bbb93c8ae182
Krishna Patel <patelkrishu1608@gmail.com>
2025-10-19 11:44:42 +0530

    Error handling

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import dbPool from './config/db.js';
 import projectRoute from './src/routes/projectRoute.js';
 import dashboardRoute from './src/routes/dashboardRoute.js';
 import documentRoute from './src/routes/documentRoute.js'
-import financeRoute from './src/routes/financeRoute.js';
+import financeRoutes from './src/routes/financeRoute.js';
 import cors from 'cors';
 const app=express();
 const PORT=process.env.PORT;
@@ -36,7 +36,7 @@ app.get("/ping", async (req, res) => {
 app.use("/api",projectRoute);
 app.use("/api",dashboardRoute);
 app.use("/api/documents", documentRoute);
-app.use("/api/finance", financeRoute);
+app.use("/api/finance", financeRoutes);
 
 
 app.listen(PORT || 3000,()=>{
